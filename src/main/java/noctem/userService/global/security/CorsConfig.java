e47d229e65e10d2fb5fe218ad11b68ffd7bc295d
arotein <9cksqhdel3@naver.com>
2022-09-30 11:11:37 +0900

    chore(global.security): fix corsFilter

--- src/main/java/noctem/userService/global/security/CorsConfig.java
+++ src/main/java/noctem/userService/global/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         // 리액트에서 헤더받을 수 있게 설정
-        config.setExposedHeaders(Arrays.asList("Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"));
+        config.setExposedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
         // AllowCredentials이 true일 때 AllowedOrigin 사용못하므로 AllowedOriginPattern으로 대체
         config.addAllowedOriginPattern("*");
